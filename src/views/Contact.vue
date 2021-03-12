7a1a646a981ce2773349add9e067784436c41a68
maxwell-clark <maxwell6636@gmail.com>
2021-03-12 03:12:28 -0600

    laid the foundation for the contacts page. also researched emailjs

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,6 +2,17 @@
   <div>
       <!-- <SidePanel /> -->
       <h2>This is for my Contact Page</h2>
+      <form>
+        <h3>Your Email</h3>
+        <input type="email" v-model="senderEmail" placeholder="Can't wait to hear from you!">
+        <h3>Your Name</h3>
+        <input type="text" v-model="senderName">
+        <h3>Subject</h3>
+        <input type="text" v-model="emailSubject" placeholder="What's Up?">
+        <h3>Email Body</h3>
+        <input type="email" v-model="senderEmail" placeholder=":)">
+        <button>Submit</button>
+      </form>
   </div>
 </template>
 
@@ -11,8 +22,31 @@
 
 export default {
   name: 'Contact',
+  data() {
+    return{
+      senderEmail: null,
+      emailSubject: null,
+      emailBody: null
+    }
+  },
   components: {
-    // SidePanel
+    
+  },
+  methods: {
+    // sendEmail() { 
+    //   Email.send({ 
+    //     Host: "smtp.gmail.com", 
+    //     Username: "sender@email_address.com", 
+    //     Password: "Enter your password", 
+    //     To: 'maxwell6636@email_address.com', 
+    //     From: `${this.senderEmail}`, 
+    //     Subject: `${this.emailSubject}`, 
+    //     Body: `${this.emailBody}`, 
+    //   }) 
+    //     .then(function (message) { 
+    //       alert("mail sent successfully") 
+    //     }); 
+    // } 
   }
 }
 </script>

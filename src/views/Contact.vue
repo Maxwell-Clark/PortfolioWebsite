fe83c4859ec1e6c9a1f6632dae553864c2df5d42
maxwell-clark <maxwell6636@gmail.com>
2021-03-12 21:04:34 -0600

    set up email js and the css of the contact page, just need to do the right div

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,24 +1,37 @@
 <template>
   <div>
       <!-- <SidePanel /> -->
-      <h2>This is for my Contact Page</h2>
-      <form>
-        <h3>Your Email</h3>
-        <input type="email" v-model="senderEmail" placeholder="Can't wait to hear from you!">
-        <h3>Your Name</h3>
-        <input type="text" v-model="senderName">
-        <h3>Subject</h3>
-        <input type="text" v-model="emailSubject" placeholder="What's Up?">
-        <h3>Email Body</h3>
-        <input type="email" v-model="senderEmail" placeholder=":)">
-        <button>Submit</button>
+      <h2>Contact Me</h2>
+      <label id="sub_head">Lets talk! (or just say hi)</label>
+      <hr>
+
+    <div class="container">
+      <form class="contact_form" action="action_page.php">
+
+        <input type="text" id="fname" name="firstname" required placeholder="Your name..">
+
+        <input type="email" id="lname" name="lastname" required placeholder="Your email..">
+
+        <textarea id="subject" name="subject" required placeholder="Whats Up :)" style="height:200px"></textarea>
+
+        <input type="submit" value="Submit">
+
       </form>
+
+      <div id="metadata">
+        <label >location</label>
+        <label >location</label>
+        <label >location</label>
+        <label >location</label>
+
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
-// import SidePanel from '@/components/SidePanel.vue'
-//research how to set up an contact page
+import emailjs from 'emailjs-com';
 
 export default {
   name: 'Contact',
@@ -33,24 +46,112 @@ export default {
     
   },
   methods: {
-    // sendEmail() { 
-    //   Email.send({ 
-    //     Host: "smtp.gmail.com", 
-    //     Username: "sender@email_address.com", 
-    //     Password: "Enter your password", 
-    //     To: 'maxwell6636@email_address.com', 
-    //     From: `${this.senderEmail}`, 
-    //     Subject: `${this.emailSubject}`, 
-    //     Body: `${this.emailBody}`, 
-    //   }) 
-    //     .then(function (message) { 
-    //       alert("mail sent successfully") 
-    //     }); 
-    // } 
+    sendEmail: (e) => {
+      emailjs.sendForm('SERVICE_ID', 'TEMPLATE_ID', e.target, 'USER_ID')
+        .then((result) => {
+            console.log('SUCCESS!', result.status, result.text);
+        }, (error) => {
+            console.log('FAILED...', error);
+        });
+    }
   }
+
 }
 </script>
 
-<style>
+<style scoped>
+ #sub_head {
+    color: white;
+    align-self: center;
+    margin: 10px;
+    font-size: 1.5em;
+  }
+  #metadata {
+    margin: 10px;
+    font-size: 1.5em;
+    display: flex;
+    justify-content: center;
+    width: 20em;
+    flex-direction: column;
+  }
+  .contact_form {
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+    justify-content: space-between;
+    height: 50%;
+    width: 40em;
+    background-color: white;
+  }
+  h2 {
+    font-family: 'Libre Barcode 39 Text', cursive;
+    font-size: 5em;
+    margin: auto;
+    margin-top: 50px;
+    margin-bottom: 20px;
+  }
+  label {
+    color: #284B63;
+    margin: 10px;
+    font-family: 'Inconsolata', monospace;
+  }
+  input {
+    max-width: 30em;
+    margin: 10px;
+    color: #fff;
+    border: 2px solid transparent;
+    border-radius: 4px;
+    background-color: #284B63;
+    font-family: 'Inconsolata', monospace;
+  }
+  textarea {
+    max-width: 30em;
+    margin: 10px;
+    color: #fff;
+    background-color: #284B63;
+    font-family: 'Inconsolata', monospace;
+  }
+  ::placeholder {
+    color: white;
+    font-family: 'Inconsolata', monospace;
+    opacity: 0.85;
+  }
 
-</style>
+/* w3 css */
+
+input, select, textarea {
+  width: 100%; /* Full width */
+  padding: 12px; /* Some padding */ 
+  border: 1px solid #ccc; /* Gray border */
+  border-radius: 4px; /* Rounded borders */
+  box-sizing: border-box; /* Make sure that padding and width stays in place */
+  margin-top: 6px; /* Add a top margin */
+  margin-bottom: 16px; /* Bottom margin */
+  resize: vertical /* Allow the user to vertically resize the textarea (not horizontally) */
+}
+
+/* Style the submit button with a specific background color etc */
+input[type=submit] {
+  background-color: #284B63;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+/* When moving the mouse over the submit button, add a darker green color */
+input[type=submit]:hover {
+  background-color: #3C6E71;
+}
+
+/* Add a background color and some padding around the form */
+.container {
+  border-radius: 5px;
+  background-color: #fff;
+  display: flex;
+  justify-content: space-evenly;
+  padding: 20px;
+}
+
+</style>>
